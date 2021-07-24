ed014bd665151c32dd9411ffecceef4ea5f69418
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2021-07-24 12:49:27 +0200

    Update status.js

--- commands/developer/status.js
+++ commands/developer/status.js
@@ -3,14 +3,14 @@ const { Clieny } = require("discord.js");
 module.exports = {
 	name: "status",
 	category: "developer",
-	description: "Modifica lo stato del bot",
+	description: "Edit the bot status",
 	run: async(client, message, args) => {
 		if(!message.author.id === 652590649735905303) {
-			return message.channel.send("Solo il developer può eseguire questo comando")
+			return message.channel.send("Only developers can execute that command")
 		}else{
 			const argsnocomma = args.slice(0).join(" ")
 			client.user.setActivity(`${argsnocomma}`);
-			message.channel.send("Ok, stato impostato a " + argsnocomma);
+			message.channel.send("Ok, status seted to " + argsnocomma);
 		}
 	}
 }
